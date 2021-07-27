d2382461d379003ada8ffaaaab2e9fce1863c3d3
Ahmed Baligh <58535713+ahmedbaligh@users.noreply.github.com>
2021-07-27 22:22:50 +0200

    feat: Implement Express server and API
    Add ability to make requests to Meaning Cloud API for NLP of URLs.
    Add .env files to ignored files by Git for enhanced privacy.
    Update Webpack production configuration.

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,57 @@
-var path = require('path');
+const path = require('path');
 const express = require('express');
-const mockAPIResponse = require('./mockAPI.js');
+
+// Allow access to .env files for enhanced privacy
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
-app.use(express.static('dist'));
+// Add body parsing middleware
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
+// Add CORS to allow Cross-Origin Requests
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static('dist'));
 console.log(__dirname);
 
-app.get('/', function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve('src/client/views/index.html'));
-});
+app.get('/', (_, res) => res.sendFile('dist/index.html'));
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+// Start the express server
+const PORT = 8081;
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
-app.get('/test', function (req, res) {
-  res.send(mockAPIResponse);
+// Connecting and sending articles' links to Meaning Cloud API
+const fetch = require('node-fetch');
+const key = process.env.MEANING_CLOUD_API_KEY;
+
+// Handle Meaning Cloud API requests for NLP
+app.post('/analyze', (req, res) => {
+  const url = req.body?.url;
+  const lang = req.body?.lang ?? 'en';
+
+  let result;
+  (async () => {
+    try {
+      result = await fetch(
+        `https://api.meaningcloud.com/sentiment-2.1?lang=${lang}&key=${key}&url=${url}`,
+        {
+          method: 'POST',
+          redirect: 'follow'
+        }
+      );
+
+      result = await result.json();
+    } catch (error) {
+      result = `The following error happened, while making your request: ${error}`;
+    } finally {
+      res.send(result);
+    }
+  })();
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,29 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const WorkBoxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
   mode: 'production',
+  output: {
+    clean: true,
+    libraryTarget: 'var',
+    library: 'Client'
+  },
   module: {
     rules: [
       {
         test: '/.js$/',
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
   },
@@ -18,6 +31,11 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/client/views/index.html',
       filename: './index.html'
-    })
-  ]
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+    new WorkBoxPlugin.GenerateSW()
+  ],
+  optimization: {
+    minimizer: [new TerserPlugin({}), new CssMinimizerPlugin({})]
+  }
 };
