6d4ba590f15836bb5b3de9b0885b0f8acd3c9eca
Ahmed Baligh <58535713+ahmedbaligh@users.noreply.github.com>
2021-07-28 10:03:49 +0200

    feat: Implement new UI & change analysis language
    Add new styles to the app.
    Add the ability to control the analysis language.

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,7 @@ import './styles/base.scss';
 import './styles/header.scss';
 import './styles/footer.scss';
 import './styles/form.scss';
+import './styles/results.scss';
 
 const form = document.querySelector('#form');
 
