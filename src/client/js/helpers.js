64d0d5303a9be2fa7be86bdf7be77a1adbb9ce4b
Ahmed Baligh <58535713+ahmedbaligh@users.noreply.github.com>
2021-07-29 20:17:21 +0200

    refactor: Improve code logic and readability

--- src/client/js/helpers.js
+++ src/client/js/helpers.js
@@ -1,6 +1,4 @@
 export const onValidate = (form, isValid) => {
   form.querySelector('[type="submit"]').disabled = !isValid;
-
-  if (isValid) form.classList.remove('invalid');
-  else form.classList.add('invalid');
+  return form.classList.toggle('invalid', !isValid);
 };
